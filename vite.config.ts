b8a00f0d9dbb4493a074d627a71bb94519f55d31
Gabriel Karajallo <c.gabrielkarajallo@gmail.com>
2026-07-21 10:19:44 +0200

    cambio vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portafolio/',
   server: {
     port: process.env.PORT ? Number(process.env.PORT) : 5173,
   },
